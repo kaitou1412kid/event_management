b167ca92f343ebf1e181f45e32c03789d8adc788
kaitou1412kid <kritagyashakya0@gmail.com>
2023-10-18 14:23:14 +0545

    minor changes

--- event/eventApp/models.py
+++ event/eventApp/models.py
@@ -9,13 +9,13 @@ class Event(models.Model):
     time = models.TimeField()
     location = models.CharField(max_length=255)
     category = models.CharField(max_length=50)
-    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
     
 class RSVP(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     rsvp_status = models.CharField(
         max_length=10,
